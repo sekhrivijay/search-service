78e26c6dd6b5aff0c5b06ac26aa30f89aecefe42
FTDCORP\cdegreef <cdegreef@ftdi.com>
2018-01-05 13:49:39 -0600

    add dev1 environment.  check http return code on tests

--- src/test/java/com/sears/search/generic/service/impl/QueryServiceImplTest.java
+++ src/test/java/com/sears/search/generic/service/impl/QueryServiceImplTest.java
@@ -1,7 +1,12 @@
 package com.sears.search.generic.service.impl;
 
-import com.micro.services.search.api.response.SearchServiceResponse;
-import com.micro.services.search.ServiceApplication;
+import static org.springframework.http.HttpStatus.OK;
+
+import java.net.URL;
+
+import javax.inject.Inject;
+
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -11,33 +16,70 @@ import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import javax.inject.Inject;
-import java.net.URL;
+import com.micro.services.search.ServiceApplication;
+import com.micro.services.search.api.response.SearchServiceResponse;
 
+/**
+ * General failures of these tests may be correctable by adding
+ * -Dspring.profiles.active=dev1. "dev1" is a value that can be found in the
+ * resources folder as a suffix to one of the spring configuration application-?
+ * files.
+ */
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT, classes = ServiceApplication.class)
 public class QueryServiceImplTest {
 
     @Value("${server.port:8080}")
-    private int port;
+    private int              port;
 
-    private URL base;
+    private URL              base;
 
     @Inject
     private TestRestTemplate template;
 
     @Before
     public void setUp() throws Exception {
-        this.base = new URL("http://localhost:" + port + "/generic-search/query?q=*:*");
+        this.base = new URL("http://localhost:" + port + "/api/search");
     }
 
     @Test
-    public void testGetHello() throws Exception {
+    public void searchForEverything() throws Exception {
+        final Long expectedNumberFound = new Long(17);
+        ResponseEntity<SearchServiceResponse> response = template.getForEntity(
+                base.toString() + "?q=*:*",
+                SearchServiceResponse.class);
+        Assert.assertEquals("checking for 200 OK", OK, response.getStatusCode());
+        Assert.assertEquals("number found", expectedNumberFound, response.getBody().getNumFound());
+    }
+
+    @Test
+    public void searchForNothing2() throws Exception {
+        final Long expectedNumberFound = new Long(13);
+        ResponseEntity<SearchServiceResponse> response = template.getForEntity(
+                base.toString() + "?q=:*",
+                SearchServiceResponse.class);
+        Assert.assertEquals("checking for 200 OK", OK, response.getStatusCode());
+        Assert.assertEquals("number found", expectedNumberFound, response.getBody().getNumFound());
+    }
+
+    @Test
+    public void searchForNothing() throws Exception {
+        final Long expectedNumberFound = new Long(13);
         ResponseEntity<SearchServiceResponse> response = template.getForEntity(
                 base.toString(),
                 SearchServiceResponse.class);
-        System.out.println(response);
-//        assertThat(response.getBody(), equalTo("Greetings from Spring Boot!"));
+        Assert.assertEquals("checking for 200 OK", OK, response.getStatusCode());
+        Assert.assertEquals("number found", expectedNumberFound, response.getBody().getNumFound());
+    }
+
+    @Test
+    public void searchForNonExistentProducts() throws Exception {
+        final Long expectedNumberFound = new Long(0);
+        ResponseEntity<SearchServiceResponse> response = template.getForEntity(
+                base.toString() + "?q=ThisIsNotAProduct",
+                SearchServiceResponse.class);
+        Assert.assertEquals("checking for 200 OK", OK, response.getStatusCode());
+        Assert.assertEquals("number found", expectedNumberFound, response.getBody().getNumFound());
     }
 
 }
